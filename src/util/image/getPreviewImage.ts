654106861bbc09f86ab25f3b034e3f11e84db742
Lawrence Behar <lbehar@xtivia.com>
2026-03-18 08:50:50 -0500

    It looks like minifier doesn't respect optional chaining operator

--- src/util/image/getPreviewImage.ts
+++ src/util/image/getPreviewImage.ts
@@ -1,10 +1,13 @@
 export const getPreviewImage = () => {
-    const parentEl:HTMLDivElement = document?.querySelector(
-        "#portlet_com_liferay_journal_web_portlet_JournalPortlet, .page-editor__topper.active",
+    console.log('latest version');
+    let previewImage: HTMLImageElement | null = null;
+    const parentEl: HTMLDivElement = document?.querySelector(
+        "#portlet_com_liferay_journal_web_portlet_JournalPortlet, .page-editor__topper.active"
     );
-    const previewImage:HTMLImageElement = parentEl?.querySelector(
-        ".page-editor__topper.active img.page-editor__editable, .image-picker-preview>img",
-    ) as HTMLImageElement;
-    
+    if(parentEl) {
+        previewImage = parentEl.querySelector(
+            ".page-editor__topper.active img.page-editor__editable, .image-picker-preview>img"
+        ) as HTMLImageElement;
+    }
     return previewImage;
 }
